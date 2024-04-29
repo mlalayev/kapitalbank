2e3041fcc98062df8df031ba39301d6975d1f300
Murad Lalayev <lalayevmurad@gmail.com>
2024-04-29 19:50:41 +0400

    my commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from './components/header/header'
+import Home from './components/home/home.jsx'
 
 function App() {
 
   return (
     <>
       <Header />
+      <Home />
     </>
   )
 }
